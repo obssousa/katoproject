b38d5bb7e6d914749569f5657b45dad80e106a99
Bruno Sousa <bssbrubs@gmail.com>
2023-01-03 20:58:37 -0300

    feature: add footer

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,27 @@ import NavBar from '@/components/NavBar.vue'
       <NavBar />
       <el-main class="main"><router-view></router-view></el-main>
     </el-container>
+     <el-footer class="footer"><a target="_blank" href="https://obssousa.github.io">Desenvolvido por @obssousa</a></el-footer>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
-.header {
-  width: 100%;
-
-  .logo {
-      height: 50px;
-  }
-}
-
 .main {
   padding: 0px;
+}
+
+.footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to left, var(--el-color-white), var(--el-fill-color-darker));
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
 }
 
 .flex-grow {
