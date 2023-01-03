6c3b1c224b00a2c122cfb822cf59087e35c0e8cf
Bruno Sousa <bssbrubs@gmail.com>
2023-01-03 20:53:51 -0300

    feature: add mobile navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,6 @@
-<script setup>
-</script>
-
 <template>
     <el-menu
+        v-if="!isMobile"
         :default-active="activeIndex"
         class="header"
         mode="horizontal"
@@ -16,12 +14,39 @@
         <el-menu-item index="3">Federação</el-menu-item>
         <el-menu-item index="4">Parceiros</el-menu-item>
     </el-menu>
+    <el-menu
+        v-if="isMobile"
+        :default-active="activeIndex"
+        class="header"
+        mode="horizontal"
+        :ellipsis="false"
+    >
+        <img @click="navigateToHash(0)" class="logo" src="@/assets/kato.png" />
+        <div class="divider"></div>
+        <el-button
+            icon="Menu"
+            size="large"
+            class="menu"
+            circle
+            @click="drawer = true"
+        />
+    </el-menu>
+    <el-drawer v-model="drawer" direction="btt" :with-header="false">
+        <div class="drawer">
+            <el-button class="button" @click="navigateToHash(1)" link type="primary">Ensino</el-button>
+            <el-button class="button" @click="navigateToHash(2)" link type="primary">Unidades</el-button>
+            <el-button class="button" @click="navigateToHash(3)" link type="primary">Federação</el-button>
+            <el-button class="button" @click="navigateToHash(4)" link type="primary">Parceiros</el-button>
+        </div>
+    </el-drawer>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import useDevice from '@/hooks/useDevice'
 
+const drawer = ref(false)
 const router = useRouter()
 const activeIndex = ref('0')
 const navItem = reactive([
@@ -31,7 +56,13 @@ const navItem = reactive([
 function navigateToHash (index) {
   activeIndex.value = index
   router.push({ path: '/', hash: `#${navItem[index]}` })
+
+  if (isMobile.value) {
+    drawer.value = false
+  }
 }
+
+const { isMobile } = useDevice()
 
 </script>
 
@@ -55,6 +86,22 @@ function navigateToHash (index) {
     .divider {
         max-width: 1080px;
         flex-grow: 1;
+    }
+
+    .menu {
+        margin-right: 16px;
+        margin-top: 12px;
+        border: none;
+    }
+}
+
+.drawer {
+    display: flex;
+    flex-direction: column;
+
+    .button {
+        height: 35px;
+        margin: 0;
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
 import './assets/element/index.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 app.mount('#app')
