b1052311454d07bea776c504427970a904dfc5cc
Bruno Sousa <bssbrubs@gmail.com>
2023-01-04 19:46:59 -0300

    fix: remove webhost branding

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,6 @@ import NavBar from '@/components/NavBar.vue'
 
 <style lang="scss" scoped>
 
-img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]  {
-  display: none;
-}
-
 .main {
   padding: 0px;
 }
